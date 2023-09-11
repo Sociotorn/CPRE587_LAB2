53ecc6f685c5581f69f1599dbc9bed7756412218
duwe <duwe@linuxvdi-01.ece.iastate.edu>
2023-09-11 14:38:55 -0500

    Fixed namespace semicolons and cosnt typo.

--- src/layers/Layer.cpp
+++ src/layers/Layer.cpp
@@ -22,4 +22,4 @@ bool LayerParams::isCompatible(const LayerParams& params) const {
 // Ensure that data being inputted is of the correct size and shape that the layer expects
 bool Layer::checkDataInputCompatibility(const LayerData& data) const { return inParams.isCompatible(data.getParams()); }
 
-};  // namespace ML
+}  // namespace ML
